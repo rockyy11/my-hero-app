05e26726c8d97f4fb7cdf307b8407b60f8545f07
Samar Atoliya <samaratoliya@Samars-MacBook-Pro.local>
2019-01-04 18:28:45 +0530

    Heroes listing with Services

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'APJ Kalam'},
+  {id: 2, name: 'Atal Bihari Vajpaye'},
+  {id: 3, name: 'Sardar Patel'},
+  {id: 4, name: 'Lala Rajpat Rai'},
+  {id: 5, name: 'Maharana Pratap'},
+  {id: 6, name: 'Pranab Mukherji'},
+  {id: 7, name: 'Kabir Das'},
+  {id: 8, name: 'Laxmi Bai'},
+  {id: 9, name: 'Bhagat Singh'},
+  {id: 10, name: 'Rajendra Prasad'}
+];
